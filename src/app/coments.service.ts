8a534d1e44ec7709cfd097845ae0cd2e5792c4a0
ChuckConde <chuck.conde@gmail.com>
2017-10-26 21:04:20 -0300

    comentario¿?

--- src/app/coments.service.ts
+++ src/app/coments.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
+import {Http} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class ComentsService {
 
-  constructor() { }
+  constructor(private http: Http) { }
+  etAll():Observable<any>{
+    return this.http.get('https://jsonplaceholder.typicode.com/comments');
 
+}
 }
--- src/app/coments/coments.component.ts
+++ src/app/coments/coments.component.ts
@@ -2,17 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import {ComentsService} from '../coments.service';
 
 
+
+
 @Component({
   selector: 'app-coments',
   templateUrl: './coments.component.html',
   styleUrls: ['./coments.component.scss']
 })
 export class ComentsComponent implements OnInit {
-
-
-  constructor() { }
-
+coment: any;
+ComentsService: any;
+constructor (private comentService: ComentsService) {}
+   
   ngOnInit() {
-  }
-
+  
+  this.ComentsService.getAll().subscribe(coment=>{
+  this.coment = coment.json();
+  })
+}
 }
